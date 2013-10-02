4c03484f4fb42642b9799a8a782fd9a1c78bfd6f
Daniel Graziotin <dgraziotin@task3.cc>
2013-10-02 16:56:36 +0200

    Permalink made readonly

--- js/singleresult.js
+++ js/singleresult.js
@@ -72,7 +72,7 @@ function showResult(json) {
             $('#journal-issn').text('ISSN: ' + journalISSN);
             var permalink = window.location.origin +'/#' + journalISSN;
             window.location.hash = journalISSN;
-            $('#journal-permalink').html('Permalink to this entry: <input type="text" id="permalink" class="form-control" style="width:70%;display:inline" value="' +permalink +'">');
+            $('#journal-permalink').html('Permalink to this entry: <input type="text" id="permalink" class="form-control" style="width:70%;display:inline" value="' +permalink +'" readonly="readonly">');
             copyrightLinkURL = 'http://www.sherpa.ac.uk/romeo/issn/' + journalISSN + '/';
         }
 
