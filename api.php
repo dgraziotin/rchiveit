236d6caaf2208a6d36066c85534a2bd3ab8962ed
Daniel Graziotin <dgraziotin@task3.cc>
2013-10-05 12:03:45 +0200

    SHERPA/RoMEO results are cached for 7 days [Fixes #9]

--- api.php
+++ api.php
@@ -1,8 +1,7 @@
 <?php
-header("Content-Type: application/xml; charset=utf-8");
 
-if( !isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) || !( $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest' ) )
-    exit();
+//if( !isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) || !( $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest' ) )
+//exit();
 
 if(file_exists('config.php'))
 	include 'config.php';
@@ -10,35 +9,55 @@ if(file_exists('config.php'))
 if(!isset($api_key))
 	$api_key = 'please set an API key';
 
-if(!isset($_GET["searchByWhat"]) || !isset($_GET["searchByWhat"]))
+if(!isset($_GET["searchByWhat"]) || !isset($_GET["searchValue"]))
 	exit();
 
-$bywhat = $_GET["searchByWhat"];
+$searchByWhat = trim(strtolower($_GET["searchByWhat"]));
+$searchValue = trim(strtolower($_GET["searchValue"]));
+
 $url = '';
 
-switch($bywhat){
+switch($searchByWhat){
 	case 'by-issn':
-		$issn = $_GET["searchValue"];
-		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&qtype=exact&ak=".$api_key."&issn=".$issn;
+		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&qtype=exact&ak=".$api_key."&issn=".$searchValue;
 	break;
 	case 'by-id':
-		$id = $_GET["searchValue"];
-		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&ak=".$api_key."&id=".$id;
+		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&ak=".$api_key."&id=".$searchValue;
 	break;
 	case 'by-journal':
-		$journal_name = $_GET["searchValue"];
-		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&qtype=contains&ak=".$api_key."&jtitle=".$journal_name;
+		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&qtype=contains&ak=".$api_key."&jtitle=".$searchValue;
 	break;
 	case 'by-publisher':
-		$publisher_name = $_GET["searchValue"];
-		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&qtype=exact&ak=".$api_key."&pub=".$publisher_name;
+		$url = "http://www.sherpa.ac.uk/romeo/api29.php?versions=all&qtype=exact&ak=".$api_key."&pub=".$searchValue;
 	break;
 	default:
 		exit();
 }
 
-$xml = file_get_contents($url);
-$data = iconv("ISO-8859-1", "UTF-8", $xml);
-echo ($data);
+
+$hash = md5($url);
+$filename = $hash.".xml";
+
+$cacheFile = dirname( __FILE__ ).DIRECTORY_SEPARATOR."cache".DIRECTORY_SEPARATOR.$filename;
+$cacheExpiresDays = 7 * (24 * 60 * 60); // Expire Time (7 Days)
+
+if (file_exists($cacheFile) && (time() - filectime($cacheFile)) < $cacheExpiresDays ){
+    // cache file exists and is not older than 5 days
+    // serve it instead of doing an API call
+    $xml = file_get_contents($cacheFile);
+}else{
+    // cache file does not exist or is older than $cacheExpiresDays
+    // make API call and save it.
+    $xml =  iconv("ISO-8859-1", "UTF-8", file_get_contents($url));
+    if($xml){
+        file_put_contents($cacheFile, $xml);
+    }else{
+        unlink($cacheFile);
+    }
+}
+
+
+header("Content-Type: application/xml; charset=utf-8");
+echo ($xml);
 
 ?>
